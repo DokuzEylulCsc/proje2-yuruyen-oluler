8c4f27b04b90178dd84683eb7de4613aad7e4afb
simgeakosman <simge.akosman@ogr.deu.edu.tr>
2019-05-26 13:33:13 +0300

    .........

--- HotelReservation/HotelReservation/Forms/AdminÖzet.cs
+++ HotelReservation/HotelReservation/Forms/AdminÖzet.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace HotelReservation.Forms
 {
-    public partial class AdminÖzet : Button
+    public partial class AdminÖzet : Form
     {
         public AdminÖzet()
         {
